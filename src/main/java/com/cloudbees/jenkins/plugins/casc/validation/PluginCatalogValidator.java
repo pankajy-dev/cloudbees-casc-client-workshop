d1d75aeac5a92bb0e4ff088cbc8ec937152fe077
Emilio Escobar <escoem@users.noreply.github.com>
2022-02-08 11:49:51 +0100

    [BEE-6356] Test added

--- src/main/java/com/cloudbees/jenkins/plugins/casc/validation/PluginCatalogValidator.java
+++ src/main/java/com/cloudbees/jenkins/plugins/casc/validation/PluginCatalogValidator.java
@@ -6,11 +6,14 @@ import com.cloudbees.jenkins.cjp.installmanager.casc.validation.PlainBundle;
 import com.cloudbees.jenkins.cjp.installmanager.casc.validation.Validation;
 import com.cloudbees.jenkins.cjp.installmanager.casc.validation.ValidationCode;
 import com.cloudbees.jenkins.plugins.assurance.remote.BeekeeperRemote;
+import net.sf.json.JSONObject;
 import org.kohsuke.accmod.restrictions.suppressions.SuppressRestrictedWarnings;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -48,13 +51,32 @@ public class PluginCatalogValidator extends AbstractValidator {
     }
 
     private List<String> validate(String catalog) {
+
+        String json = yaml2json(catalog);
+        if (json == null) {
+            return Arrays.asList("The catalog file content is not valid");
+        }
+
         boolean oldAllow = BeekeeperRemote.get().isCapExceptionsAllowed();
         BeekeeperRemote.get().setCapExceptionsAllowed(true);
 
-        List<String> validationErrors = BeekeeperRemote.get().validateExtension(catalog, null);
+        List<String> validationErrors = BeekeeperRemote.get().validateExtension(json, null);
 
         BeekeeperRemote.get().setCapExceptionsAllowed(oldAllow);
 
         return validationErrors;
     }
+
+    private String yaml2json(String yaml) {
+        Map<String,Object> object = parseYaml(yaml);
+
+        if (object == null) {
+            return null;
+        }
+
+        JSONObject json = JSONObject.fromObject(object);
+
+        return json != null ? json.toString() : null;
+    }
+
 }
