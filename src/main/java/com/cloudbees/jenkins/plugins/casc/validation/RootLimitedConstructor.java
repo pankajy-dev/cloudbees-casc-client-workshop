1fda94097af40efeec5e6dff27cf2203975e7b92
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2022-05-06 09:19:30 +0200

    [BEE-17161] Add validators extensions

---
+++ src/main/java/com/cloudbees/jenkins/plugins/casc/validation/RootLimitedConstructor.java
@@ -0,0 +1,25 @@
+package com.cloudbees.jenkins.plugins.casc.validation;
+
+import org.yaml.snakeyaml.constructor.Constructor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class RootLimitedConstructor extends Constructor {
+
+    private static final Logger LOGGER = Logger.getLogger(RootLimitedConstructor.class.getName());
+
+    private Class<? extends Object> theRoot;
+    public RootLimitedConstructor(Class<? extends Object> theRoot) {
+        super(theRoot);
+        this.theRoot = theRoot;
+    }
+    @Override
+    protected Class<?> getClassForName(String name) throws ClassNotFoundException {
+        if (!this.theRoot.getName().equals(name)) {
+            LOGGER.log(Level.WARNING, "Blocking class deserialization for security reasons: [{0}]", name);
+            throw new ClassNotFoundException("refusing to unmarshal " + name);
+        }
+        return super.getClassForName(name);
+    }
+}
