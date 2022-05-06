7d08889b9f7aab75ed824ccb60c211beeec9faf9
Pedro Bueno <pbuenoyerbes@cloudbees.com>
2022-05-06 12:11:45 +0200

    [BEE-17161] added items validation

---
+++ src/main/java/com/cloudbees/jenkins/plugins/casc/validation/ItemsValidatorExtension.java
@@ -0,0 +1,100 @@
+package com.cloudbees.jenkins.plugins.casc.validation;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.jenkinsci.plugins.variant.OptionalExtension;
+import org.kohsuke.accmod.restrictions.suppressions.SuppressRestrictedWarnings;
+
+import com.cloudbees.jenkins.cjp.installmanager.casc.BundleLoader;
+import com.cloudbees.jenkins.cjp.installmanager.casc.validation.PathPlainBundle;
+import com.cloudbees.jenkins.cjp.installmanager.casc.validation.Validation;
+import com.cloudbees.jenkins.cjp.installmanager.casc.validation.ValidationCode;
+import com.cloudbees.jenkins.plugins.casc.items.validation.ItemsValidator;
+
+/**
+ * Performs validations on items to create / update in a running instance
+ */
+@OptionalExtension(requirePlugins = "configuration-as-code")
+@SuppressRestrictedWarnings(value = { BundleLoader.class})
+public class ItemsValidatorExtension extends AbstractValidator{
+
+    private static final Logger LOGGER = Logger.getLogger(ItemsValidatorExtension.class.getName());
+
+    @Override
+    public ValidationCode getCode() {
+        return ValidationCode.ITEMS_DEFINITION;
+    }
+
+    @Override
+    public List<Validation> validate(Path bundlePath) {
+        if (bundlePath == null) {
+            return Collections.emptyList();
+        }
+
+        BundleLoader.BundleDescriptor descriptor = null;
+        try {
+            descriptor = readDescriptor(bundlePath.resolve("bundle.yaml"));
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Error parsing the bundle descriptor", e);
+            return Collections.emptyList(); // No need to return errors,
+        }
+
+        if (descriptor == null) {
+            LOGGER.warning("Empty bundle descriptor");
+            return Collections.singletonList(error("The bundle.yaml file seems to be empty. Validations cannot be performed."));
+        }
+
+        List<String> items = descriptor.getItems();
+        if (items == null || items.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Validation> errors = new ArrayList<>();
+        List<String> filesNotFound = new ArrayList<>();
+        List<String> filesUnparseable = new ArrayList<>();
+        for (String file : items) {
+            Path path = bundlePath.resolve(file);
+            if (!Files.exists(path)) {
+                filesNotFound.add(file);
+            } else {
+                try {
+                    Map<String, Object> parsed = parseYaml(path);
+                    if (parsed == null || parsed.isEmpty()) {
+                        filesUnparseable.add(file);
+                    }
+                } catch (IOException e) {
+                    filesUnparseable.add(file);
+                }
+            }
+        }
+        if (!filesNotFound.isEmpty()) {
+            String notFound = filesNotFound.stream().collect(Collectors.joining(", "));
+            errors.add(error(String.format("The bundle.yaml file references %s in the Items section that cannot be found. Impossible to validate items.", notFound)));
+        }
+        if (!filesUnparseable.isEmpty()) {
+            String unparseable = filesUnparseable.stream().collect(Collectors.joining(", "));
+            errors.add(error(String.format("The bundle.yaml file references %s in the Items section that is empty or has an invalid yaml format. Impossible to validate items.", unparseable)));
+        }
+
+        if (!errors.isEmpty()) {
+            return Collections.unmodifiableList(errors);
+        }
+
+        ItemsValidator validator = new ItemsValidator();
+        Collection<Validation> validations = validator.validate(new PathPlainBundle(bundlePath));
+        if (!validations.isEmpty()) {
+            LOGGER.log(Level.WARNING, String.format("Some items could not be created: %s", validations.stream().map(x -> x.getMessage()).collect(Collectors.joining(","))));
+        }
+        return validations.stream().collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/cloudbees/jenkins/plugins/casc/validation/ItemsValidatorExtensionTest.java
@@ -0,0 +1,57 @@
+package com.cloudbees.jenkins.plugins.casc.validation;
+
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import hudson.ExtensionList;
+
+import com.cloudbees.jenkins.cjp.installmanager.casc.validation.Validation;
+import com.cloudbees.jenkins.cjp.installmanager.casc.validation.ValidationCode;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class ItemsValidatorExtensionTest {
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+    @Rule
+    public TestName testName = new TestName();
+
+    @Test
+    public void smokes() {
+        ItemsValidatorExtension validator = ExtensionList.lookupSingleton(ItemsValidatorExtension.class);
+
+        List<Validation> validations = validator.validate(Paths.get("src/test/resources/com/cloudbees/jenkins/plugins/casc/validation/bundles/JCasCValidatorExtensionTest/missing-file"
+                                                          + "-bundle"));
+        Validation v = validations.get(0);
+        assertThat("missing-file-bundle: should be an error in JCASC", v.getLevel(), is(Validation.Level.ERROR));
+        assertThat("missing-file-bundle: should be an error in JCASC", v.getValidationCode(), is(ValidationCode.ITEMS_DEFINITION));
+        assertThat("missing-file-bundle: should be an error in JCASC", v.getMessage(), is("[ITEMS] - The bundle.yaml file references items.yaml in the Items section that "
+                                                                                          + "cannot be found. Impossible to validate items."));
+
+        validations = validator.validate(Paths.get("src/test/resources/com/cloudbees/jenkins/plugins/casc/validation/bundles/JCasCValidatorExtensionTest/unparsed-file-bundle"));
+        v = validations.get(0);
+        assertThat("unparsed-file-bundle: should be an error in JCASC", v.getLevel(), is(Validation.Level.ERROR));
+        assertThat("unparsed-file-bundle: should be an error in JCASC", v.getValidationCode(), is(ValidationCode.ITEMS_DEFINITION));
+        assertThat("unparsed-file-bundle: should be an error in JCASC", v.getMessage(), is("[ITEMS] - The bundle.yaml file references items.yaml in the Items section that is "
+                                                                                           + "empty or has an invalid yaml format. Impossible to validate items."));
+
+        validations = validator.validate(Paths.get("src/test/resources/com/cloudbees/jenkins/plugins/casc/validation/bundles/ItemsValidatorExtensionTest/with-items/"));
+        assertThat("We should get validation results containing an entry", validations, hasSize(1));
+        assertThat("It should be a warning", validations.get(0).getLevel(), is(Validation.Level.WARNING));
+        assertThat("It should be a warning", validations.get(0).getValidationCode(), is(ValidationCode.ITEMS_DEFINITION));
+        assertThat("It should contain all not valid items", validations.get(0).getMessage(), allOf(containsString("pipeline"), containsString("clientController"),
+                                                                                              containsString("not-a-valid-type")));
+        assertThat("It should not contain allowed items", validations.get(0).getMessage(), not(anyOf(containsString("freeStyle"), containsString("folder"))));
+    }
+}
