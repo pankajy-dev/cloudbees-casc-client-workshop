440ac67485425e820495076aa301325d32797cd8
Pedro Bueno <pbuenoyerbes@cloudbees.com>
2022-08-25 09:45:44 +0200

    [BEE-22192] removing no longer used try-catch and error message

--- src/main/java/com/cloudbees/opscenter/client/casc/HotReloadAction.java
+++ src/main/java/com/cloudbees/opscenter/client/casc/HotReloadAction.java
@@ -7,13 +7,9 @@ import hudson.model.RootAction;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import javax.annotation.CheckForNull;
-import javax.servlet.ServletException;
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -52,21 +48,9 @@ public class HotReloadAction implements RootAction {
     public HttpResponse doReload() {
         Jenkins.get().checkPermission(Jenkins.MANAGE);
         BundleReloadAction realAction = ExtensionList.lookupSingleton(BundleReloadAction.class);
-        try {
-            if (!realAction.tryReload(true)){
-                LOGGER.log(Level.INFO, "Configuration Bundle hot reload has been requested but the current bundle can not be reloaded");
-            }
-            return HttpResponses.redirectViaContextPath("/manage");
-        } catch (Exception e) {
-            LOGGER.warning(() -> "An error occurred while reloading the configuration:\n" + e);
-            return new HttpResponse() {
-                @Override
-                public void generateResponse(StaplerRequest staplerRequest, StaplerResponse staplerResponse, Object o) throws IOException, ServletException {
-                    staplerRequest.setAttribute("hot_reload_error", e.getMessage());
-                    staplerRequest.getView(ExtensionList.lookupSingleton(HotReloadAction.class), "index")
-                            .forward(staplerRequest, staplerResponse);
-                }
-            };
+        if (!realAction.tryReload(true)){
+            LOGGER.log(Level.INFO, "Configuration Bundle hot reload has been requested but the current bundle can not be reloaded");
         }
+        return HttpResponses.redirectViaContextPath("/manage");
     }
 }
