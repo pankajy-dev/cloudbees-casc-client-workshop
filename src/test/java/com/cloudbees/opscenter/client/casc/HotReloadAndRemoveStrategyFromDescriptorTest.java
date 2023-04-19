d9faaad7c96a37d316689f02dafe417b68579a13
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2023-04-19 19:42:29 +0200

    [BEE-33295] Test all potential use cases

--- src/test/java/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest.java
+++ src/test/java/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest.java
@@ -47,11 +47,11 @@ public class HotReloadAndRemoveStrategyFromDescriptorTest extends AbstractCJPTes
         rule.waitForCompletion(fsp1.scheduleBuild2(0).waitForStart());
         rule.waitForCompletion(fsp2.scheduleBuild2(0).waitForStart());
         rule.waitForCompletion(fsp3.scheduleBuild2(0).waitForStart());
-
         assertThat("free-root must have a build", fsp1.getBuilds(), not(empty()));
         assertThat("folder-root/free-in-folder must have a build", fsp2.getBuilds(), not(empty()));
         assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build", fsp3.getBuilds(), not(empty()));
 
+        // Version 2 declares the remove-all in the bundle descriptor, so it must override the none strategy from the items.yaml
         System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-2").toFile().getAbsolutePath());
         ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
         ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
@@ -68,6 +68,128 @@ public class HotReloadAndRemoveStrategyFromDescriptorTest extends AbstractCJPTes
         assertThat("free-root mustn't have a build as it has been recreated", fsp1.getBuilds().size(), is(0));
         assertThat("folder-root/free-in-folder mustn't have a build as it has been recreated", fsp2.getBuilds().size(), is(0));
         assertThat("folder-root/folder-in-folder/free-in-folder-in-folder mustn't have a build as it has been recreated", fsp3.getBuilds().size(), is(0));
+
+        // Creating builds to test the removals
+        rule.waitForCompletion(fsp1.scheduleBuild2(0).waitForStart());
+        rule.waitForCompletion(fsp2.scheduleBuild2(0).waitForStart());
+        rule.waitForCompletion(fsp3.scheduleBuild2(0).waitForStart());
+        assertThat("free-root must have a build", fsp1.getBuilds(), not(empty()));
+        assertThat("folder-root/free-in-folder must have a build", fsp2.getBuilds(), not(empty()));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build", fsp3.getBuilds(), not(empty()));
+
+        // Version 3 doesn't set the remove strategy but introduces a change in the items.yaml, so it must be reloaded
+        // remove strategy is none, so the removed item will remain and another will have a change
+        // none of them is removed, so the builds remain
+        System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-3").toFile().getAbsolutePath());
+        ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
+        ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
+        await().atMost(Duration.ofSeconds(60)).until(() -> !ConfigurationStatus.INSTANCE.isCurrentlyReloading());
+
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        fsp2 = Jenkins.get().getItemByFullName("folder-root/free-in-folder", FreeStyleProject.class);
+        fsp3 = Jenkins.get().getItemByFullName("folder-root/folder-in-folder/free-in-folder-in-folder", FreeStyleProject.class);
+
+        assertNotNull("free-root should have remained", fsp1);
+        assertNotNull("free-in-folder should have remained and updated", fsp2);
+        assertThat("free-in-folder should have remained and updated", fsp2.getDescription(), is("changed!"));
+        assertNotNull("free-in-folder-in-folder should have remained", fsp3);
+
+        assertThat("free-root must have a build as it hasn't been recreated", fsp1.getBuilds().size(), is(1));
+        assertThat("folder-root/free-in-folder must have a build as it hasn't been recreated", fsp2.getBuilds().size(), is(1));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build as it hasn't been recreated", fsp3.getBuilds().size(), is(1));
+
+        // Version 4 doesn't set the remove strategy but introduces a change in the items.yaml, so it must be reloaded
+        // remove strategy is sync, so the removed item won't remain and another will have a change
+        // Only one job is removed, so the builds remain
+        System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-4").toFile().getAbsolutePath());
+        ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
+        ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
+        await().atMost(Duration.ofSeconds(60)).until(() -> !ConfigurationStatus.INSTANCE.isCurrentlyReloading());
+
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        fsp2 = Jenkins.get().getItemByFullName("folder-root/free-in-folder", FreeStyleProject.class);
+        fsp3 = Jenkins.get().getItemByFullName("folder-root/folder-in-folder/free-in-folder-in-folder", FreeStyleProject.class);
+
+        assertNull("free-root should have been removed", fsp1);
+        assertNotNull("free-in-folder should have remained and updated", fsp2);
+        assertThat("free-in-folder should have remained and updated", fsp2.getDescription(), is("changed again!"));
+        assertNotNull("free-in-folder-in-folder should have remained", fsp3);
+
+        assertThat("folder-root/free-in-folder must have a build as it hasn't been recreated", fsp2.getBuilds().size(), is(1));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build as it hasn't been recreated", fsp3.getBuilds().size(), is(1));
+
+        // Version 5 doesn't set the remove strategy and doesn't introduce a change in the items.yaml, but as the remove strategy is sync
+        // it must be reloaded
+        // remove strategy is sync, so the removed item won't remain and another will have a change (will create another for the test)
+        // Only one job is removed, so the builds remain
+        rule.createFreeStyleProject("free-root");
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        assertNotNull("Just created", fsp1);
+
+        System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-5").toFile().getAbsolutePath());
+        ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
+        ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
+        await().atMost(Duration.ofSeconds(60)).until(() -> !ConfigurationStatus.INSTANCE.isCurrentlyReloading());
+
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        fsp2 = Jenkins.get().getItemByFullName("folder-root/free-in-folder", FreeStyleProject.class);
+        fsp3 = Jenkins.get().getItemByFullName("folder-root/folder-in-folder/free-in-folder-in-folder", FreeStyleProject.class);
+
+        assertNull("free-root should have been removed", fsp1);
+        assertNotNull("free-in-folder should have remained and updated", fsp2);
+        assertThat("free-in-folder should have remained and updated", fsp2.getDescription(), is("changed again!"));
+        assertNotNull("free-in-folder-in-folder should have remained", fsp3);
+
+        assertThat("folder-root/free-in-folder must have a build as it hasn't been recreated", fsp2.getBuilds().size(), is(1));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build as it hasn't been recreated", fsp3.getBuilds().size(), is(1));
+
+        // Version 6 sets the remove strategy and doesn't introduce a change in the items.yam
+        // remove strategy from descriptor is none and it must prevail over sync (from items.yaml)
+        // there's no change, so nothing is reloaded
+        // remove strategy is none, so the removed item will remain (will create another for the test)
+        // builds remain
+        rule.createFreeStyleProject("free-root");
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        assertNotNull("Just created", fsp1);
+
+        System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-6").toFile().getAbsolutePath());
+        ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
+        ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
+        await().atMost(Duration.ofSeconds(60)).until(() -> !ConfigurationStatus.INSTANCE.isCurrentlyReloading());
+
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        fsp2 = Jenkins.get().getItemByFullName("folder-root/free-in-folder", FreeStyleProject.class);
+        fsp3 = Jenkins.get().getItemByFullName("folder-root/folder-in-folder/free-in-folder-in-folder", FreeStyleProject.class);
+
+        assertNotNull("free-root should have remained", fsp1);
+        assertNotNull("free-in-folder should have remained", fsp2);
+        assertNotNull("free-in-folder-in-folder should have remained", fsp3);
+
+        assertThat("folder-root/free-in-folder must have a build as it hasn't been recreated", fsp2.getBuilds().size(), is(1));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build as it hasn't been recreated", fsp3.getBuilds().size(), is(1));
+
+        // Version 7 sets the remove strategy and introduces a change in the items.yam
+        // remove strategy from descriptor is none and it must prevail over sync (from items.yaml)
+        // there's a change, so it must be reflected
+        // remove strategy is none, so the removed item will remain (created for version 6)
+        // builds remain
+        System.setProperty("core.casc.config.bundle", Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/HotReloadAndRemoveStrategyFromDescriptorTest/version-7").toFile().getAbsolutePath());
+        ExtensionList.lookupSingleton(BundleVisualizationLink.class).doBundleUpdate(); // Force the bundle update
+        ExtensionList.lookupSingleton(BundleReloadAction.class).tryReload(); // Reload the bundle
+        await().atMost(Duration.ofSeconds(60)).until(() -> !ConfigurationStatus.INSTANCE.isCurrentlyReloading());
+
+        fsp1 = Jenkins.get().getItemByFullName("free-root", FreeStyleProject.class);
+        fsp2 = Jenkins.get().getItemByFullName("folder-root/free-in-folder", FreeStyleProject.class);
+        fsp3 = Jenkins.get().getItemByFullName("folder-root/folder-in-folder/free-in-folder-in-folder", FreeStyleProject.class);
+
+        assertNotNull("free-root should have remained", fsp1);
+        assertNotNull("free-in-folder should have remained and updated", fsp2);
+        assertThat("free-in-folder should have remained and updated", fsp2.getDescription(), is("changed again and again!"));
+        assertNotNull("free-in-folder-in-folder should have remained", fsp3);
+
+        assertThat("folder-root/free-in-folder must have a build as it hasn't been recreated", fsp2.getBuilds().size(), is(1));
+        assertThat("folder-root/folder-in-folder/free-in-folder-in-folder must have a build as it hasn't been recreated", fsp3.getBuilds().size(), is(1));
+
     }
 
     public static final class TwoPluginsV2dot289 implements TestEnvelopeProvider {
