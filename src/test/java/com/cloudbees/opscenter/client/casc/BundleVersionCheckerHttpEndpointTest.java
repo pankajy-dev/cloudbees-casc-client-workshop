9ef9b73d33e14cb3b70a2d867b691b745e7ab093
Jean-Marc Desprez <jmdesprez@cloudbees.com>
2023-07-03 12:03:41 +0200

    [BEE-35011] Add tests for casc-bundle-mgnt/check-bundle-update

--- src/test/java/com/cloudbees/opscenter/client/casc/BundleVersionCheckerHttpEndpointTest.java
+++ src/test/java/com/cloudbees/opscenter/client/casc/BundleVersionCheckerHttpEndpointTest.java
@@ -15,8 +15,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
 
+import static com.cloudbees.opscenter.client.casc.CasCMatchers.hasInfoMessage;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 public class BundleVersionCheckerHttpEndpointTest extends AbstractBundleVersionCheckerTest {
@@ -118,6 +121,72 @@ public class BundleVersionCheckerHttpEndpointTest extends AbstractBundleVersionC
         assertUpdateAvailable(jsonResult, "version-11.zip", true);
         assertVersions(jsonResult, "version-11.zip", "9", empty(), "11", empty(), true);
         assertUpdateType(jsonResult, "version-11.zip", "RELOAD");
+    }
+
+    @Test
+    @WithEnvelope(TestEnvelope.class)
+    @WithConfigBundle("src/test/resources/com/cloudbees/opscenter/client/casc/AbstractBundleVersionCheckerTest/version-2.zip")
+    public void testQuietMode() throws IOException {
+        CJPRule.WebClient wc = rule.createWebClient();
+        WebResponse resp;
+        JSONObject jsonResult;
+
+        // Check version 4 - Invalid - no param
+        String version4Path = Paths.get("src/test/resources/com/cloudbees/opscenter/client/casc/AbstractBundleVersionCheckerTest/version-4.zip")
+                                   .toFile().getAbsolutePath();
+        System.setProperty("core.casc.config.bundle", version4Path);
+        resp = requestWithToken(HttpMethod.GET, new URL(rule.getURL(), "casc-bundle-mgnt/check-bundle-update"), admin, wc);
+        jsonResult = JSONObject.fromObject(resp.getContentAsString());
+
+        assertThat("Current version should contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("current-bundle")
+                             .getJSONArray("validations"),
+                   hasInfoMessage()
+        );
+        assertThat("Current version should contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("new-version")
+                             .getJSONArray("validations"),
+                   hasInfoMessage()
+        );
+
+        // Check version 4 - Invalid - not quiet
+        System.setProperty("core.casc.config.bundle", version4Path);
+        resp = requestWithToken(HttpMethod.GET, new URL(rule.getURL(), "casc-bundle-mgnt/check-bundle-update?quiet=false"), admin, wc);
+        jsonResult = JSONObject.fromObject(resp.getContentAsString());
+
+        assertThat("Current version should contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("current-bundle")
+                             .getJSONArray("validations"),
+                   hasInfoMessage()
+        );
+        assertThat("Current version should contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("new-version")
+                             .getJSONArray("validations"),
+                   hasInfoMessage()
+        );
+
+        // Check version 4 - Invalid - Quiet
+        System.setProperty("core.casc.config.bundle", version4Path);
+        resp = requestWithToken(HttpMethod.GET, new URL(rule.getURL(), "casc-bundle-mgnt/check-bundle-update?quiet=true"), admin, wc);
+        jsonResult = JSONObject.fromObject(resp.getContentAsString());
+
+        assertThat("Current version should not contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("current-bundle")
+                             .getJSONArray("validations"),
+                   not(hasInfoMessage())
+        );
+        assertThat("New version should not contains INFO messages",
+                   jsonResult.getJSONObject("versions")
+                             .getJSONObject("new-version")
+                             .getJSONArray("validations"),
+                   not(hasInfoMessage())
+        );
+
     }
 
     private WebResponse requestWithToken(HttpMethod method, URL fullURL, User asUser, CJPRule.WebClient wc)
